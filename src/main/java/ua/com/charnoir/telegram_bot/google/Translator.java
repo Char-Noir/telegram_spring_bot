abc691493fed27d55c554a7684e3d8927fda010d
AnnYat <AnnYat@BitBucket.com>
2022-02-14 01:45:08 +0200

    fix Translator

--- src/main/java/ua/com/charnoir/telegram_bot/google/Translator.java
+++ src/main/java/ua/com/charnoir/telegram_bot/google/Translator.java
@@ -15,10 +15,10 @@ import java.nio.charset.StandardCharsets;
 public class Translator {
 
     @Value("${google.translator.url}")
-    private static String url;
+    private String url;
 
-    public static String translate( String langTo, String text) throws IOException {
-
+    public String translate(String langTo, String text) throws IOException {
+        text = text.replace("\n"," ❤ ");
         String urlStr = url +
                 "?q=" + URLEncoder.encode(text, StandardCharsets.UTF_8) +
                 "&target=" + langTo +
@@ -27,12 +27,14 @@ public class Translator {
         StringBuilder response = new StringBuilder();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        con.setRequestMethod("GET");
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+            response.append(inputLine).append("\n");
         }
         in.close();
-        return response.toString();
+        //TODO:Fix '\n' issue; (translate api just removes this symbol)
+        return response.toString().replace("❤ ","\n");
     }
 }
--- src/main/java/ua/com/charnoir/telegram_bot/persistense/entity/user/type/Language.java
+++ src/main/java/ua/com/charnoir/telegram_bot/persistense/entity/user/type/Language.java
@@ -1,15 +1,21 @@
 package ua.com.charnoir.telegram_bot.persistense.entity.user.type;
 
 public enum Language {
-    UKR("uk_ua"), ENG("en_us"), RUS("ru_ru");
+    UKR("uk_ua", "uk"), ENG("en_us", "en"), RUS("ru_ru", "ru");
 
     private final String locale;
+    private final String translatorKey;
 
-    Language(String locale) {
+    Language(String locale, String translatorKey) {
         this.locale = locale;
+        this.translatorKey = translatorKey;
     }
 
     public String getLocale() {
         return locale;
+    }
+
+    public String getTranslator() {
+        return translatorKey;
     }
 }
