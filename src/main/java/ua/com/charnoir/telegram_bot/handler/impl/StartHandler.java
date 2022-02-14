1285da822fc51039dfa11702f1f884261fafa349
AnnYat <AnnYat@BitBucket.com>
2022-02-14 02:01:35 +0200

    add admin keyboard panel

--- src/main/java/ua/com/charnoir/telegram_bot/handler/impl/StartHandler.java
+++ src/main/java/ua/com/charnoir/telegram_bot/handler/impl/StartHandler.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import ua.com.charnoir.telegram_bot.handler.CommandHandler;
 import ua.com.charnoir.telegram_bot.handler.TextHandler;
+import ua.com.charnoir.telegram_bot.persistense.entity.user.User;
 import ua.com.charnoir.telegram_bot.persistense.entity.user.type.BotStateType;
 import ua.com.charnoir.telegram_bot.persistense.entity.user.type.Language;
-import ua.com.charnoir.telegram_bot.persistense.entity.user.User;
+import ua.com.charnoir.telegram_bot.persistense.entity.user.type.Status;
 import ua.com.charnoir.telegram_bot.persistense.repository.UserRepository;
 import ua.com.charnoir.telegram_bot.util.BundleUtil;
 
@@ -36,6 +39,15 @@ public class StartHandler implements TextHandler, CommandHandler {
         // Приветствуем пользователя
         SendMessage welcomeMessage = createMessageTemplate(user);
         welcomeMessage.setText(String.format(BundleUtil.getString(user.getLanguage(), "register"), user.getName()));
+        ReplyKeyboardMarkup markup = ReplyKeyboardMarkup.builder().build();
+        if (user.getStatus() == Status.ADMIN) {
+            KeyboardRow keyboardRow1 = new KeyboardRow();
+            keyboardRow1.add("/add_category");
+            KeyboardRow keyboardRow2 = new KeyboardRow();
+            keyboardRow2.add("/categories");
+            markup.setKeyboard(List.of(keyboardRow1, keyboardRow2));
+            welcomeMessage.setReplyMarkup(markup);
+        }
         // Просим назваться
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
         List<InlineKeyboardButton> inlineKeyboardButtonsRowOne = List.of(
@@ -47,10 +59,9 @@ public class StartHandler implements TextHandler, CommandHandler {
         SendMessage languageMessage = createMessageTemplate(user);
         languageMessage.setText(String.format(BundleUtil.getString(user.getLanguage(), "language_chos")));
         languageMessage.setReplyMarkup(inlineKeyboardMarkup);
-        // Меняем пользователю статус на - "читание"
+        // Меняем пользователю статус на - "начальный"
         user.setBotState(BotStateType.NONE);
         userRepository.save(user);
-
         return List.of(welcomeMessage, languageMessage);
     }
 
